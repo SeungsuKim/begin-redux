ad869d401669d4904b6764c464a311a2a72329fb
seungsukim <seungsu0407@gmail.com>
2019-04-27 12:02:38 +0900

    redux

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,1 +1,34 @@
-// 리덕스와 연동된 컨테이너 컴포넌트 작성
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Counter from "components/Counter";
+import { CounterActions } from "store/actionCreators";
+
+class CounterContainer extends Component {
+
+  handleIncrement = () => {
+    CounterActions.increment();
+  }
+
+  handleDecrement = () => {
+    CounterActions.decrement();
+  }
+
+  render() {
+    const { handleIncrement, handleDecrement } = this;
+    const { number } = this.props;
+
+    return (
+      <Counter
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+        number={number}
+      />
+    );
+  }
+}
+
+export default connect(
+  (state) => ({
+    number: state.counter.number
+  })
+)(CounterContainer);
